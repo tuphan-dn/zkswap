8a7c3100186ee70b1d5663a81c7a2a2f65f3cc73
tuphan-dn <tuphan@descartes.network>
2022-03-11 14:59:44 +0700

    legacy deps

--- src/view/home/index.tsx
+++ src/view/home/index.tsx
@@ -1,10 +1,13 @@
-import { Row, Col, Typography } from 'antd'
+import { Row, Col, Typography, Button } from 'antd'
 
 const Home = () => {
   return (
     <Row gutter={[24, 24]}>
       <Col span={24}>
         <Typography.Text>Home</Typography.Text>
+      </Col>
+      <Col span={24}>
+        <Button type="primary">Test</Button>
       </Col>
     </Row>
   )
