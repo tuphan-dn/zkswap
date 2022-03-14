6455f1c5a5d222cedcaaa4e7d3bcc387cc46502e
The Vinh <vinhnguyen@descartes.network>
2022-03-14 10:24:35 +0700

    create settings pop-up

--- src/components/settings/index.tsx
+++ src/components/settings/index.tsx
@@ -1,5 +1,15 @@
-import { Button, Col, Divider, Popover, Row, Typography } from 'antd'
-import { SettingOutlined } from '@ant-design/icons'
+import {
+  Button,
+  Col,
+  Divider,
+  Popover,
+  Row,
+  Tooltip,
+  Typography,
+  Switch,
+} from 'antd'
+import { SettingOutlined, WarningOutlined } from '@ant-design/icons'
+import IconSax from 'components/iconsax'
 
 const Settings = () => {
   return (
@@ -9,15 +19,79 @@ const Settings = () => {
           placement="bottomRight"
           overlayInnerStyle={{ width: 300 }}
           content={
-            <Row gutter={[8, 8]}>
+            <Row gutter={[12, 12]}>
               <Col span={24}>
                 <Typography.Title level={5}>Settings</Typography.Title>
               </Col>
-              <Col span={24}>Slippage</Col>
+              <Col span={24}>
+                <Row gutter={[8, 8]}>
+                  <Col flex="auto">
+                    <Typography.Text type="secondary" className="caption">
+                      Slippage Tolerance
+                    </Typography.Text>
+                  </Col>
+                  <Col>
+                    <Tooltip
+                      placement="topLeft"
+                      title="Tooltip text"
+                      arrowPointAtCenter
+                    >
+                      <Button
+                        type="text"
+                        shape="circle"
+                        size="small"
+                        icon={<IconSax name="InfoCircle"></IconSax>}
+                      ></Button>
+                    </Tooltip>
+                  </Col>
+                </Row>
+              </Col>
+              <Col span={24}>
+                <Col span={24}>
+                  <Row gutter={[8, 8]} justify="space-between">
+                    <Typography.Text>0.1%</Typography.Text>
+                    <Typography.Text>0.1%</Typography.Text>
+                    <Typography.Text>0.1%</Typography.Text>
+                    <Typography.Text>0.1%</Typography.Text>
+                    <Typography.Text type="danger">Freely</Typography.Text>
+                  </Row>
+                </Col>
+              </Col>
               <Divider style={{ margin: 0 }} />
-              <Col span={24}>Advanced</Col>
+              <Col span={24}>
+                <Row>
+                  <Col flex="auto">
+                    <Typography.Text>Advanced Mode</Typography.Text>
+                  </Col>
+                  <Col>
+                    <Switch size="small" />
+                  </Col>
+                </Row>
+              </Col>
+              <Col span={24}>
+                <Typography.Paragraph className="caption">
+                  <WarningOutlined />
+                  &nbsp;This advanced mode will disable the automatic protection
+                  that often results in bad rates and lost funds. Only use this
+                  mode if you know what you are doing.
+                </Typography.Paragraph>
+              </Col>
               <Divider style={{ margin: 0 }} />
-              <Col span={24}>Enhancement</Col>
+              <Col span={24}>
+                <Row>
+                  <Col flex="auto">
+                    <Typography.Text>Enhancement UI</Typography.Text>
+                  </Col>
+                  <Col>
+                    <Switch size="small" />
+                  </Col>
+                </Row>
+              </Col>
+              <Col span={24}>
+                <Typography.Paragraph  className="caption">
+                  Take advantage of all the familiar tools.
+                </Typography.Paragraph>
+              </Col>
             </Row>
           }
           trigger="click"
